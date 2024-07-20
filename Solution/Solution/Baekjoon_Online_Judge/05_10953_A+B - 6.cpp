5ada128a13cc9d97c0c4f91ebb07d18f724f956c
woochae <japchae114@gmail.com>
2024-07-20 18:19:21 +0900

    05_10953_A+B - 6

---
+++ Solution/Solution/Baekjoon_Online_Judge/05_10953_A+B - 6.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int iNumT, iNumA, iNumB;
+    char chComma;
+
+    cin >> iNumT;
+
+    while (iNumT--)
+    {
+        cin >> iNumA >> chComma >> iNumB;
+        cout << iNumA + iNumB << '\n';
+    }
+
+    return 0;
+}
