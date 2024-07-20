58aee97264748b93de075ea510a40a5d10b94c62
woochae <japchae114@gmail.com>
2024-07-20 18:09:48 +0900

    03_A+B - 4

---
+++ Solution/Solution/Baekjoon_Online_Judge/03_10951_A+B - 4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int iNumA, iNumB;
+
+    while (cin >> iNumA >> iNumB)
+        cout << iNumA + iNumB << '\n';
+
+    return 0;
+}
