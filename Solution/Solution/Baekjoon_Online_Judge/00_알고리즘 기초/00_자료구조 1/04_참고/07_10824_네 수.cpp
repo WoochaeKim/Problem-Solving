f595cb9be6fd748635e59ec01a602129624a9790
woochae <japchae114@gmail.com>
2024-07-21 22:22:24 +0900

    10824_네 수

--- Solution/Solution/Baekjoon_Online_Judge/00_알고리즘 기초/00_자료구조 1/04_참고/07_10824_네 수.cpp
+++ Solution/Solution/Baekjoon_Online_Judge/00_알고리즘 기초/00_자료구조 1/04_참고/07_10824_네 수.cpp
@@ -1,5 +1,12 @@
+// Notes
+// 자료형 범위를 항상 유의할 것!
+
 #include <iostream>
-#include <>
+#include <cmath>
+
+#define INPUT_LIMIT 4
+#define BASE 10
+
 using namespace std;
 
 int main()
@@ -8,7 +15,17 @@ int main()
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
+	int iUpper, iLower;
+	int iCntLoop{ INPUT_LIMIT / 2 };
+	long long iSum{};
 
+	while (iCntLoop--)
+	{
+		cin >> iUpper >> iLower;
+		iSum += iUpper * static_cast<long long>(pow(BASE, floor(log10(iLower)) + 1)) + iLower;
+	}
+
+	cout << iSum;
 
 	return 0;
 }
