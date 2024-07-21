0715360d218246c437ba0e2787cf6d29ac7a4c26
woochae <japchae114@gmail.com>
2024-07-21 18:26:01 +0900

    10808_알파벳 개수

--- Solution/Solution/Baekjoon_Online_Judge/00_알고리즘 기초/00_자료구조 1/04_참고/02_10809_알파벳 찾기.cpp
+++ Solution/Solution/Baekjoon_Online_Judge/00_알고리즘 기초/00_자료구조 1/04_참고/02_10809_알파벳 찾기.cpp
@@ -9,17 +9,10 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
-
+	
 	string strInput;
 	array<int, 'z' - 'a' + 1> arrBuff{};
 
-	cin >> strInput;
-
-	for (char chElem : strInput)
-		arrBuff[chElem - 'a'] += 1;
-
-	for (int iElem : arrBuff)
-		cout << iElem << ' ';
 
 	return 0;
 }
