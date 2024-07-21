ec8fe2e917628767df865c9ded02d35bc29896eb
woochae <japchae114@gmail.com>
2024-07-21 21:29:15 +0900

    11655_ROT13

--- Solution/Solution/Baekjoon_Online_Judge/00_알고리즘 기초/00_자료구조 1/04_참고/06_11655_ROT13.cpp
+++ Solution/Solution/Baekjoon_Online_Judge/00_알고리즘 기초/00_자료구조 1/04_참고/06_11655_ROT13.cpp
@@ -1,3 +1,10 @@
+// Notes
+// ROT의 의미는 알파벳 전체 개수의 절반을 의미한다.
+// 주의할 점은 오프 바이 원 에러(Off-by-One Error)이다.
+// 두 값을 간격(Interval)과 개수(Count)를 착각하지 말자.
+// 간격(Interval) : 두 값 사이의 차이. (Z - A)
+// 개수(Count) : 두 값과 그 사이 값을 포함한 개수. (Z - A + 1)
+
 #include <iostream>
 #include <string>
 
