040a1cc5f9cb85d7bff62394dd6936c603e03b92
woochae <japchae114@gmail.com>
2024-07-23 05:13:25 +0900

    1929_소수 구하기

---
+++ Solution/Solution/Baekjoon_Online_Judge/00_알고리즘 기초/02_수학 1/수학 1/04_1929_소수 구하기.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie();
+	cout.tie();
+	
+	int iLower, iUpper;
+	vector<bool> vecChkPrime;
+	vector<int> vecBuffPrime;
+	
+	cin >> iLower >> iUpper;
+
+	vecChkPrime.resize(iUpper + 1, true);
+
+	for (int iIdxChkPrime = 2; iIdxChkPrime <= iUpper; iIdxChkPrime++)
+	{
+		if (true == vecChkPrime[iIdxChkPrime])
+		{
+			if (iLower <= iIdxChkPrime)
+				vecBuffPrime.push_back(iIdxChkPrime);
+			for (int iMultiple = iIdxChkPrime + iIdxChkPrime; iMultiple <= iUpper; iMultiple += iIdxChkPrime)
+				vecChkPrime[iMultiple] = false;
+		}
+	}
+
+	for (int Prime : vecBuffPrime)
+		cout << Prime << '\n';
+
+	return 0;
+}
