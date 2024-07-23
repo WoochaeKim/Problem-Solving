5a48af63a6ba285f0d89e1dead33386f47121e78
woochae <japchae114@gmail.com>
2024-07-23 21:01:27 +0900

    1008_A/B

---
+++ Solution/Solution/Baekjoon_Online_Judge/01. solved.ac/00. 새싹/01. 입력과 계산/03_1008.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int iInputA, iInputB;
+
+	cin >> iInputA >> iInputB;
+	cout << fixed << setprecision(9) << static_cast<double>(iInputA) / static_cast<double>(iInputB);
+
+	return 0;
+}
