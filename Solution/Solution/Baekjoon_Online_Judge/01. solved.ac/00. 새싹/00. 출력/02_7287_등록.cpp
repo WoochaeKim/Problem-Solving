56eb29d61d25767847a50b7c211a049f0bda4db1
woochae <japchae114@gmail.com>
2024-07-23 19:54:27 +0900

    7287_등록

---
+++ Solution/Solution/Baekjoon_Online_Judge/01. solved.ac/00. 새싹/00. 출력/02_7287_등록.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cout << 44 << '\n' << "japchae114";
+
+	return 0;
+}
