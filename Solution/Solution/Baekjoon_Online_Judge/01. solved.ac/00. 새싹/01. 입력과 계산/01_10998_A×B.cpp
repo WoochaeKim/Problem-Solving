71b2dde0eb6b29709fb497913904526cde2144a0
woochae <japchae114@gmail.com>
2024-07-23 20:47:01 +0900

    10998_A×B

---
+++ Solution/Solution/Baekjoon_Online_Judge/01. solved.ac/00. 새싹/01. 입력과 계산/01_10998_A×B.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int iInputA, iInputB;
+
+	cin >> iInputA >> iInputB;
+	cout << iInputA * iInputB;
+
+	return 0;
+}
