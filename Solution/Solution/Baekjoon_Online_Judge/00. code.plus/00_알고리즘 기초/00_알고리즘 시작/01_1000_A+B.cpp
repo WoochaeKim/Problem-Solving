13ae97ee8f11530fd884755549f2ae45ca71396f
woochae <japchae114@gmail.com>
2024-10-24 20:58:45 +0900

    01_1000_A+B

---
+++ Solution/Solution/Baekjoon_Online_Judge/00. code.plus/00_알고리즘 기초/00_알고리즘 시작/01_1000_A+B.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int iInputNumA, iInputNumB;
+
+	cin >> iInputNumA >> iInputNumB;
+	cout << iInputNumA + iInputNumB;
+
+	return 0;
+}
