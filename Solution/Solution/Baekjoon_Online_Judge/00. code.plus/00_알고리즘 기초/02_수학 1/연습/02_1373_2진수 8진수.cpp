12acc2ddaf24630c1e45a112617bade2adfd3d71
woochae <japchae114@gmail.com>
2024-10-24 19:55:39 +0900

    Reset_241024

---
+++ Solution/Solution/Baekjoon_Online_Judge/00. code.plus/00_알고리즘 기초/02_수학 1/연습/02_1373_2진수 8진수.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <iostream>
+
+//#define PIVOT_EXP 3
+
+#define JUINS DUNGJUN
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cout << JUINS;
+
+	/*
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	
+	int iExponent{};
+	string strInput;
+
+	cin >> strInput;
+
+	int iCntLoop{ static_cast<int>(strInput.size()) };
+
+	while (iCntLoop--)
+	{
+		(PIVOT_EXP <= iExponent) ? (iExponent = 0) : iExponent++;
+		strInput[iCntLoop] 
+	}
+
+
+	reverse(strInput.begin(), strInput.end());
+
+	int iLimitIdx{ ) };
+
+	for (int iIdx = 0; )
+	*/
+	return 0;
+}
