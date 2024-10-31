948093558b5c3dc92acb2b97660f124ae1221f1c
woochae <japchae114@gmail.com>
2024-10-31 17:59:46 +0900

    05_2743_단어 길이 재기

---
+++ Solution/Solution/Baekjoon_Online_Judge/00. code.plus/00_알고리즘 기초/01_자료구조 1/04_참고/05_2743_단어 길이 재기.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	string strInput{};
+	cin >> strInput;
+	cout << strInput.size();
+
+	return 0;
+}
