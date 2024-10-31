5a0485500de4de3d2a15b73c62264137c39e4809
woochae <japchae114@gmail.com>
2024-10-31 17:19:21 +0900

    02_10808_알파벳 개수

---
+++ Solution/Solution/Baekjoon_Online_Judge/00. code.plus/00_알고리즘 기초/01_자료구조 1/04_참고/02_10808_알파벳 개수.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include <string>
+#include <array>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie();
+	cout.tie();
+
+	array<int, 'z' - 'a' + 1> arrBuff{};
+
+	string strInput{};
+	getline(cin, strInput);
+
+	for (const char& chAlph : strInput)
+		++arrBuff[chAlph - 'a'];
+
+	for (const int& iNumchAlph : arrBuff)
+		cout << iNumchAlph << ' ';
+
+	return 0;
+}
