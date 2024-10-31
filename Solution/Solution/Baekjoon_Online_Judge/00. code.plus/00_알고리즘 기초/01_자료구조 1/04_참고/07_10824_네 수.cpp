52555e7301c065f3309f31d19643caa2dcdef39d
woochae <japchae114@gmail.com>
2024-10-31 20:07:15 +0900

    07_10824_네 수

---
+++ Solution/Solution/Baekjoon_Online_Judge/00. code.plus/00_알고리즘 기초/01_자료구조 1/04_참고/07_10824_네 수.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <array>
+
+using namespace std;
+
+enum TYPE_NUM { NUM_A, NUM_B, NUM_C, NUM_D, NUM_END };
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	array<string, TYPE_NUM::NUM_END> arrNumBuff{};
+
+	for (string& strNum : arrNumBuff)
+		cin >> strNum;
+
+	cout << stoi(arrNumBuff[NUM_A] + arrNumBuff[NUM_B]) + stoi(arrNumBuff[NUM_C] + arrNumBuff[NUM_D]);
+
+	return 0;
+}
