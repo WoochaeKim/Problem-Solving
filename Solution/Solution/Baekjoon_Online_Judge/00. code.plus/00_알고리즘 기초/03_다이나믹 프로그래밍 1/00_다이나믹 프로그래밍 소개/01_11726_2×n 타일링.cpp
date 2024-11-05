221abae70654ac0775387f035f9b621a051bcf7c
woochae <japchae114@gmail.com>
2024-11-05 23:27:09 +0900

    01_11726_2×n 타일링

---
+++ Solution/Solution/Baekjoon_Online_Judge/00. code.plus/00_알고리즘 기초/03_다이나믹 프로그래밍 1/00_다이나믹 프로그래밍 소개/01_11726_2×n 타일링.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> g_vecMemo{};
+
+int NumOfWays(int _iNum)
+{
+	if (0 > _iNum)
+		return 0;
+
+	if (0 < g_vecMemo[_iNum])
+		return g_vecMemo[_iNum];
+
+	g_vecMemo[_iNum] = NumOfWays(_iNum - 1) + NumOfWays(_iNum - 2);
+
+	return g_vecMemo[_iNum];
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int iWidth{};
+	cin >> iWidth;
+
+	g_vecMemo.resize(iWidth + 1);
+	g_vecMemo[0] = 1, g_vecMemo[1] = 1;
+
+	cout << NumOfWays(iWidth);// % 10007;
+
+	return 0;
+}
