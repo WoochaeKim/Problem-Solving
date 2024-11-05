167183afaedda48bc2015d8405d55b7d9c00fcb1
woochae <japchae114@gmail.com>
2024-11-05 12:06:45 +0900

    04_2089_-2진수

--- Solution/Solution/Baekjoon_Online_Judge/00. code.plus/00_알고리즘 기초/02_수학 1/연습/03_1212_8진수 2진수.cpp
+++ Solution/Solution/Baekjoon_Online_Judge/00. code.plus/00_알고리즘 기초/02_수학 1/연습/03_1212_8진수 2진수.cpp
@@ -10,13 +10,12 @@ int main()
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
-	array<string, 8> arrOctToBin{ "000", "001", "010", "011", "100", "101", "110", "111" };
+	const array<string, 8> arrOctToBin{ "000", "001", "010", "011", "100", "101", "110", "111" };
 
 	string strInput{};
 	cin >> strInput;
 
 	int iStrSize{ static_cast<int>(strInput.size()) };
-	//int iDigitOctal{  };
 
 	cout << stoi(arrOctToBin[static_cast<int>(strInput.front() - '0')]);
 
@@ -25,33 +24,6 @@ int main()
 		for (int iCntidx{ 1 }; iCntidx < iStrSize; ++iCntidx)
 			cout << arrOctToBin[static_cast<int>(strInput[iCntidx] - '0')];
 	}
-	/*
 
-	if (0 == iNumOctal && 1 == iStrSize)
-		cout << iNumOctal;
-	else
-	{
-		bool bIsFirstOne{};
-
-		for (int iCntBP{ 4 }; iCntBP > 0; iCntBP >>= 1)
-		{
-			if (!bIsFirstOne && iNumOctal < iCntBP)
-				continue;
-			else
-				bIsFirstOne = true;
-
-			cout << ((0 > iNumOctal - iCntBP) ? 0 : (iNumOctal -= iCntBP, 1));
-		}
-
-		for (int iCntidx{ 1 }; iCntidx < iStrSize; ++iCntidx)
-		{
-			iNumOctal = static_cast<int>(strInput[iCntidx] - '0');
-
-			for (int iCntBP{ 4 }; iCntBP > 0; iCntBP >>= 1)
-				cout << ((0 > iNumOctal - iCntBP) ? 0 : (iNumOctal -= iCntBP, 1));
-		}
-	}
-
-	*/
 	return 0;
 }
