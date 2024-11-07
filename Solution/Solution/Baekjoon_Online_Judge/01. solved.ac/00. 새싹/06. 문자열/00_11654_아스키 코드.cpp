a5e8297e07fb4881aa206da0eb4c79896dfbdcd5
woochae <japchae114@gmail.com>
2024-11-07 21:45:14 +0900

    00_11654_아스키 코드

---
+++ Solution/Solution/Baekjoon_Online_Judge/01. solved.ac/00. 새싹/06. 문자열/00_11654_아스키 코드.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	char chInput{};
+	cin >> chInput;
+
+	cout << static_cast<int>(chInput);
+
+	return 0;
+}
