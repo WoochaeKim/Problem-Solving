5b00956bfd0e5e3886f0db0d5bac699d6a25a817
woochae <japchae114@gmail.com>
2024-11-08 11:36:03 +0900

    10_2920_음계

---
+++ Solution/Solution/Baekjoon_Online_Judge/01. solved.ac/01. CLASS 1/10_2920_음계.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int iNumInput{}, iLastNum{}, iOffset{};
+	string strResult{ "mixed" };
+
+	cin >> iNumInput;
+
+	switch (iNumInput)
+	{
+		case 1:
+			strResult = "ascending";
+			iOffset = 1;
+			break;
+		case 8:
+			strResult = "descending";
+			iOffset = -1;
+			break;
+	}
+
+	if ("mixed" != strResult)
+	{
+		for (int iCntIdx{ 2 }; iCntIdx <= 8; ++iCntIdx)
+		{
+			iLastNum = iNumInput;
+			cin >> iNumInput;
+
+			if (iLastNum - iNumInput + iOffset)
+			{
+				strResult = "mixed";
+				break;
+			}
+		}
+	}
+
+	cout << strResult;
+
+	return 0;
+}
