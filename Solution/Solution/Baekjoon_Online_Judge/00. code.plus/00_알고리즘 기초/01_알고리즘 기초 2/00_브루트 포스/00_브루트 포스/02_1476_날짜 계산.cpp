b0b4ce36771a73900a45287858835f4068f32045
woochae <japchae114@gmail.com>
2024-11-24 22:33:10 +0900

    02_1476_날짜 계산

--- Solution/Solution/Baekjoon_Online_Judge/00. code.plus/00_알고리즘 기초/01_알고리즘 기초 2/00_브루트 포스/00_브루트 포스/02_1476_날짜 계산.cpp
+++ Solution/Solution/Baekjoon_Online_Judge/00. code.plus/00_알고리즘 기초/01_알고리즘 기초 2/00_브루트 포스/00_브루트 포스/02_1476_날짜 계산.cpp
@@ -4,7 +4,20 @@ using namespace std;
 
 int main()
 {
-	
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	const int EARTH{ 15 }, SUN{ 28 }, MOON{ 19 };
+	int iEarth{}, iSun{}, iMoon{}, iYear{};
+
+	cin >> iEarth >> iSun >> iMoon;
+	--iEarth, --iSun, --iMoon;
+
+	while (!(iEarth == iYear % EARTH && iSun == iYear % SUN && iMoon == iYear % MOON))
+		++iYear;
+
+	cout << ++iYear;
 
 	return 0;
 }
